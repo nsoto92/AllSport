d80214827b3d27a70f7d887e5a50588b6ee7c2a3
khadijah faison <khadijahfaison@gmail.com>
2020-04-02 10:36:02 -0700

    added react-icons and updated nav bar names on homepage only

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { TiShoppingCart } from "react-icons/ti"
 import { FaSearch } from "react-icons/fa"
 import "bootstrap/dist/css/bootstrap.min.css"
