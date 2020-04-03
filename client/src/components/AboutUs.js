e0c463e9748b45fbe479d30f384db85fc1e1beb2
khadijah faison <khadijahfaison@gmail.com>
2020-04-03 08:48:56 -0700

    about us page--still needs work

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -2,8 +2,30 @@ import React from "react"
 
 export default props => {
   return (
-    <div>
-      <h1>about us</h1>
+    <div className="container-fluid aboutWrapper">
+      <div>
+        <img
+          className="d-block w-100"
+          src="./assets/item-2.png"
+          alt="hero image"
+        />
+      </div>
+      <div className="container text-center">
+        <div className="features">
+          <h1>Who We Are</h1>
+          <h3>
+            Get sports paraphernalia and sports memorabilia directly from your
+            favorite sports athletes & alumni. Take photos with alumni who make
+            personal deliveries to your home, office, workplace, campus or
+            school. Order autographed items and have them delivered directly to
+            you by your favorite sports athletes & alumni. Talk face-to-face
+            with your favorite sports athletes & alumni after you’ve made your
+            purchase. It is chance of a lifetime. Take it!
+            <br /> <br />
+            Sports fans cannot possibly beat this deal or miss this opportunity.
+          </h3>
+        </div>
+      </div>
     </div>
   )
 }
