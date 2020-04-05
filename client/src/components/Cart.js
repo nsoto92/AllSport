7ba1d888f499af12f11eb8a8d088579ea0cff243
Norbert Soto <nor.soto92@gmail.com>
2020-04-05 02:22:18 -0700

    cart works

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,24 +1,39 @@
 import React from "react"
 import { useItems } from "../hooks"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/products.css"
 
-import { FaShoppingCart } from "react-icons/fa"
+import { FaTimes } from "react-icons/fa"
 
 export default function(e) {
-  const { cart } = useItems()
+  const { cart, status, toggle, total, del } = useItems()
 
   return (
-    <div className="cart">
-      <FaShoppingCart />
+    <div id={status ? "cartItems" : "cartItems closed"}>
+      <FaTimes
+        onClick={toggle}
+        className={
+          status
+            ? "nav-item  rounded-circle mx-2 basket-icon"
+            : "nav-item  rounded-circle mx-2 basket-icon closed"
+        }
+      />{" "}
+      [Close]
       <h1>Cart</h1>
+      <p className="cartTitle">{cart.length} item(s) are in your cart</p>
       {cart.map(product => (
-        <div className="productcart">
-          <img src={product.image} />
-          <p>{product.title}</p>
-          <p>{product.price}</p>
+        <div className="prod" key={product.id}>
+          <img className="prodImg" src={product.image} />
+          {product.title}
+          <br />${product.price}
+          <br />
+          <button onClick={e => del(product)} className="prodCart">
+            Remove
+          </button>
         </div>
       ))}
       <div>
-        <h2>Total: 0 </h2>
+        <h3> Total: ${total}</h3>
       </div>
     </div>
   )
--- client/src/redux/ducks/cart/index.js
+++ client/src/redux/ducks/cart/index.js
@@ -1,71 +1,67 @@
-// 1. imports
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 
-// 2. action definitions
+const GET_STATUS = "cart/GET_STATUS"
 const ADD_PRODUCT = "cart/ADD_PRODUCT"
-const DEL_PRODUCT = "cart/DEL_PRODUCT"
-const TOGGLE_CART = "cart/TOGGLE_CART"
+const DELETE_PRODUCT = "cart/DELETE_PRODUCT"
 
-// 3. initial state
 const initialState = {
   cart: [],
-  isOpen: false
+  open: false
 }
-// 4. reducer
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_PRODUCT:
       return { ...state, cart: [...state.cart, action.payload] }
-    case TOGGLE_CART:
-      return { ...state, isOpen: !state.isOpen }
-    case DEL_PRODUCT:
+    case GET_STATUS:
+      return { ...state, open: !state.open }
+    case DELETE_PRODUCT:
       return {
         ...state,
-        cart: [...state.cart.filter(product => product !== action.payload)]
+        cart: [...state.cart.filter(cart => cart !== action.payload)]
       }
     default:
       return state
   }
 }
-// 5. action creators
+
+function toggleCart() {
+  return {
+    type: GET_STATUS
+  }
+}
 
 function addProduct(product) {
-  return dispatch => {
-    dispatch({
-      type: ADD_PRODUCT,
-      payload: product
-    })
+  return {
+    type: ADD_PRODUCT,
+    payload: product
   }
 }
 
 function delProduct(product) {
-  return dispatch => {
-    dispatch({
-      type: DEL_PRODUCT,
-      payload: product
-    })
-  }
-}
-
-function toggleCart() {
   return {
-    type: TOGGLE_CART
+    type: DELETE_PRODUCT,
+    payload: product
   }
 }
 
-// 6. custom hook
 export function useItems() {
   const dispatch = useDispatch()
   const cart = useSelector(appState => appState.cartState.cart)
-  const isOpen = useSelector(appState => appState.cartState.isOpen)
+  const status = useSelector(appState => appState.cartState.open)
+
+  const toggle = () => dispatch(toggleCart())
   const add = product => dispatch(addProduct(product))
   const del = product => dispatch(delProduct(product))
-  const toggle = () => dispatch(toggleCart())
-  // const total = cart.reduce((a, b) => a + b.price, 0).toFixed(2)
+
+  const [total, setTotal] = useState("")
 
   useEffect(() => {
-    console.log(cart)
-  }, [])
-  return { cart, add, isOpen, toggle, del }
+    const amount = cart.reduce((a, b) => Number(a) + Number(b.price), 0)
+
+    setTotal(amount.toFixed(2))
+  }, [cart])
+
+  return { cart, status, toggle, add, del, total }
 }
