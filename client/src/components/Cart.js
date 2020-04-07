6c9c75ef11403e4daa25fad6ac3210e8f1b0cb7f
Norbert Soto <nor.soto92@gmail.com>
2020-04-06 18:18:11 -0700

    cartStyle

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -13,27 +13,33 @@ export default function(e) {
 
   return (
     <div className={status ? "cartItems" : "cartItems closed"}>
-      <FaTimes
-        onClick={toggle}
-        className={
-          status
-            ? "nav-item  rounded-circle mx-2 basket-icon"
-            : "nav-item  rounded-circle mx-2 basket-icon closed"
-        }
-      />{" "}
       <div className="cartAS">
-        <h1>Cart</h1>
-        <p className="cartTitle">{cart.length} item(s) are in your cart</p>
+        <div className="cartHead">
+          <FaTimes
+            onClick={toggle}
+            className={
+              status
+                ? "nav-item  rounded-circle mx-2 basket-icon"
+                : "nav-item  rounded-circle mx-2 basket-icon closed"
+            }
+          />
+          <h1>Cart</h1>
+          <p className="cartTitle">
+            - You have {cart.length} item(s) in your cart
+          </p>
+        </div>
         <div className="buyItems">
           {cart.map(product => (
-            <div className="prod" key={product.id}>
-              <img className="prodImg" src={product.image} />
-              {product.title}
-              <br />${product.price}
-              <br />
-              <button onClick={e => del(product)} className="prodCart">
-                Remove
-              </button>
+            <div className="listedProd" key={product.id}>
+              <img className="prodImgCart" src={product.image} />
+              <div className="prodDetails">
+                {product.title}
+                <br />${product.price}
+                <br />
+                <button onClick={e => del(product)} className="prodCart">
+                  Remove
+                </button>
+              </div>
             </div>
           ))}
         </div>
@@ -41,7 +47,7 @@ export default function(e) {
       <div>
         <h3> Total: ${total}</h3>
         <Link to="/checkout">
-          <p>CHECKOUT</p>
+          <p className="receiptLink">CHECKOUT</p>
         </Link>
       </div>
     </div>
