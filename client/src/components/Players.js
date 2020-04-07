8bbc641e3451b1d53b03bccefecab9ba9584577b
JustinBurrows <justinburrows.software@gmail.com>
2020-04-06 23:45:03 -0700

    inprogress

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -4,7 +4,6 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { usePlayers } from "../hooks"
 import styles from "../styles/findPros.css"
-import { ScrollBox, ScrollAxes, FastTrack } from "react-scroll-box"
 
 export default (props) => {
   const { players } = usePlayers()
@@ -15,20 +14,28 @@ export default (props) => {
           <h1>Find A Pro</h1>
         </div>
       </div>
+      <div className="playerBox">
+        {players.map((player) => (
+          <div className="playerList">
+            <div>
+              <img className="findPros" src={player.img} alt="Product 1" />
+            </div>
 
-      {players.map((player) => (
-        <div className="playerList">
-          <div>
-            <img className="findPros" src={player.img} alt="Product 1" />
+            <div className="playerName">
+              <Link to={"/playerprofiles"}>
+                <p>{player.firstname + " " + player.lastname}</p>
+              </Link>
+            </div>
+            <div className="playerSport">
+              <p>{" Sport: " + player.sport + " " + " Team: " + player.team}</p>
+            </div>
+
+            <div className="playerDescriptionContainer">
+              <p className="playerDescription">{player.description}</p>
+            </div>
           </div>
-          <div className="playerName">
-            <p>{player.firstname + " " + player.lastname}</p>
-          </div>
-          <div className="playerDescriptionContainer">
-            <p className="playerDescription">{player.description}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
