3634886aef66b6dd437e07e53277211588137578
JustinBurrows <justinburrows.software@gmail.com>
2020-04-08 11:27:57 -0700

    more styles

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -3,52 +3,25 @@ import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import ReactCardFlip from "react-card-flip"
 import { Link } from "react-router-dom"
+import Card from "./Card.js"
+
 import { usePlayers } from "../hooks"
 import styles from "../styles/findPros.css"
 
 export default (props) => {
+  const [message, setMessage] = useState("")
   const { players } = usePlayers()
 
-  const [isFlipped, setIsFlipped] = useState(false)
-
-  function handleClick() {
-    setIsFlipped(!isFlipped)
-  }
   return (
-    <div className="cards">
-      {players.map((player) => (
-        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-          <div className="playerList">
-            <div>
-              <img className="findPros" src={player.img} alt="Product 1" />
-            </div>
-            <div className="playerName">
-              <Link to={"/playerprofiles"}>
-                <p>{player.firstname + " " + player.lastname}</p>
-              </Link>
-            </div>
-            <button className="flipbutton" onClick={handleClick}>
-              Click to flip
-            </button>
-          </div>
-
-          <div className="playerList">
-            <div className="playerSport">
-              <p>{" Sport: " + player.sport}</p>
-            </div>
-            <div className="playerSport">
-              <p>{" Team: " + player.team}</p>
-            </div>
-
-            <div className="playerDescriptionContainer">
-              <p className="playerDescription">{player.description}</p>
-            </div>
-            <button className="flipbutton" onClick={handleClick}>
-              Click to flip
-            </button>
-          </div>
-        </ReactCardFlip>
-      ))}
+    <div>
+      <div className="title">
+        <h1>PLAYER CARDS</h1>
+      </div>
+      <div className="cards">
+        {players.map((player) => (
+          <Card player={player} />
+        ))}
+      </div>
     </div>
   )
 }
